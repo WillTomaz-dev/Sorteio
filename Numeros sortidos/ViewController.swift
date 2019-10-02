f8bff1000edc7c24b46b31f8a9945d963e3be4a2
William Tomaz <william.silva@inmetrics.com.br>
2019-10-02 15:39:12 -0300

    Initial Commit

---
+++ Numeros sortidos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Numeros sortidos
+//
+//  Created by William Tomaz on 02/10/19.
+//  Copyright © 2019 William Tomaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
